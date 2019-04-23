2f803055837a75476132ac10d4ea9482100913c2
VrRms <vitorramos2003@gmail.com>
2019-04-23 08:07:46 -0300

    Revert "a"
    This reverts commit 83c49b80fcf414dbfefa7e5fc2d1dcdac9ddcb0c.

--- apCalculadora/apCalculadora/Form1.cs
+++ apCalculadora/apCalculadora/Form1.cs
@@ -257,8 +257,7 @@ namespace apCalculadora
                         {
                             if (Convert.ToChar(expressao.Substring(j, 1)) != '(')
                             {
-                                if (Convert.ToChar(expressao.Substring(j - 1, 1)) != ')' && 
-                                    Convert.ToChar(expressao.Substring(j, 1)) != '√')
+                                if (Convert.ToChar(expressao.Substring(j - 1, 1)) != ')' && Convert.ToChar(expressao.Substring(j, 1)) != '√')
                                 {
                                     expLetras += letras[i];
                                     i++;
